e7ffe744af1a0e71d7f670f7e2c5bb510e9e2817
Edzdw <minhtam09081999@gmail.com>
2026-03-25 11:23:11 +0700

    update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,20 +1,20 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+// import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'login',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'register',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'app/**',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Server
-  }
-];
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: 'login',
+//     renderMode: RenderMode.Prerender
+//   },
+//   {
+//     path: 'register',
+//     renderMode: RenderMode.Prerender
+//   },
+//   {
+//     path: 'app/**',
+//     renderMode: RenderMode.Server
+//   },
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Server
+//   }
+// ];
